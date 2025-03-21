ae9722cf6ae0b5afbd55b094e38e16c6630eb5b3
Anita <ani_kim@kbtu.kz>
2025-03-21 21:08:14 +0500

    pages: contacts, search, category search, salon 30%, personal acc 30%

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import {MainComponent} from "./main/main.component";
+import { PersonalAccountComponent } from './personal-account/personal-account.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { CategoryComponent } from './category/category.component';
+import { SearchComponent } from './search/search.component';
 
 export const routes: Routes = [
   {path: "", component: MainComponent},
+  {path: "account", component: PersonalAccountComponent},
+  {path: "contacts", component: ContactsComponent},
+  {path: "category", component: CategoryComponent},
+  {path: "search", component: SearchComponent}
 ];
